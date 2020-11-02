64d1eae8aeb21754b0f54dfd7474e00e373f15c4
Andy Mariños <andy_mar_rey@outlook.com>
2020-11-02 17:34:48 -0500

    se agrego slider y flechas en seccion servicios

---
+++ src/js/slider-servicios.js
@@ -0,0 +1,8 @@
+$(".services-layout__slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  prevArrow: $(".services-layout__left"),
+  nextArrow: $(".services-layout__right"),
+});
